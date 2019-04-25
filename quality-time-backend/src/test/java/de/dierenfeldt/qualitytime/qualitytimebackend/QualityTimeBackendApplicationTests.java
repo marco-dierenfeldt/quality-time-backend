5b31a9c645ffa94b316e6e781d30d2c9464e2511
TheJBoBoss <jjaszewski@web.de>
2019-04-25 16:47:04 +0200

    initial creation via https://start.spring.io/

---
+++ quality-time-backend/src/test/java/de/dierenfeldt/qualitytime/qualitytimebackend/QualityTimeBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package de.dierenfeldt.qualitytime.qualitytimebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QualityTimeBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
